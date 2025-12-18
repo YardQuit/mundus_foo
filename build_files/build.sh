16c8d4f7dac6c96368a9484796d6b9dd544ef2b0
Michael Jones <yardquit@pm.me>
2025-12-18 09:58:04 +0000

    Refactor build script for package installation and services
    Updated package installation commands and added configuration for firewalld and system services.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,28 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install --skip-unavailable -y $(cat /ctx/rpm_packages)
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# this installs package from copr repos
+dnf5 -y copr enable atim/starship
+dnf5 -y copr enable pennbauman/ports
+
+dnf5 -y install starship
+dnf5 -y install lf
+
+dnf5 -y copr disable atim/starship
+dnf5 -y copr disable pennbauman/ports
 
 #### Example for enabling a System Unit File
-
+systemctl enable tuned.service
 systemctl enable podman.socket
+systemctl enable fstrim.timer
+systemctl enable firewalld.service
+systemctl enable tailscaled.service
+
+### Change default firewalld zone
+cp /etc/firewalld/firewalld-workstation.conf /etc/firewalld/firewalld-workstation.conf.bak
+sed -i 's/DefaultZone=FedoraWorkstation/DefaultZone=drop/g' /etc/firewalld/firewalld-workstation.conf
+
+### Create missing dirs
+mkdir -p /var/spool/anacron
